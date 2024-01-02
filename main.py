03fe8fcfbf0acd8e9331764a3ba6984074f90f94
carbon-ahs <shehanuk.ahsan@gmail.com>
2024-01-02 12:06:43 +0600

    Template And file structure done.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from solutions import solution_001
 
 
 def print_hi(name):
@@ -11,6 +12,8 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('carbon-ahs')
+    print_hi("carbon-ahs")
+    solution_001.solution();
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
